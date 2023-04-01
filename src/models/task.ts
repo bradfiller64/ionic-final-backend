bd369881ed007cf223560d8504af837ac9b61721
bradfiller64 <fillerbrad64@gmail.com>
2023-03-31 22:42:47 -0700

    building task model

---
+++ src/models/task.ts
@@ -0,0 +1,30 @@
+import { InferAttributes, InferCreationAttributes, Model, DataTypes, Sequelize } from "sequelize";
+
+export class Task extends Model<InferAttributes<Task>, InferCreationAttributes<Task>>{
+    declare taskId: number;
+    declare title: string;
+    declare completed: boolean;
+}
+
+export function TaskFactory(sequelize: Sequelize) {
+    Task.init({
+        taskId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        completed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        }
+    }, {
+        freezeTableName: true,
+        tableName: 'tasks',
+        sequelize
+    });
+}
