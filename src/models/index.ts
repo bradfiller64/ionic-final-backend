49b32871bfae257edc2de846b2449a0dd0d1d9ee
bradfiller64 <fillerbrad64@gmail.com>
2023-03-31 22:52:41 -0700

    finished index

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,13 @@ import { TaskFactory } from "./task";
 const dbName = 'taskDB';
 const username = 'root';
 const password = 'password';
+
+const sequelize = new Sequelize(dbName, username, password, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+TaskFactory(sequelize);
+
+export const db = sequelize;
